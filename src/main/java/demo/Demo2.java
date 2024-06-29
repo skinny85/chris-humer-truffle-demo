4c7c1860c0e01d56ffe9f1b6fad28e88ddb4a021
Adam Ruka <adamruka85@gmail.com>
2024-06-29 16:54:19 -0700

    Update demo nr 2, similarly to demo 1

--- src/main/java/demo/Demo2.java
+++ src/main/java/demo/Demo2.java
@@ -3,31 +3,26 @@ package demo;
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 
 public class Demo2 {
-
     abstract static class Expression extends Node {
-
         abstract int execute(int[] arguments);
-
     }
-   
-    static class Abs extends Expression {
 
+    static class Abs extends Expression {
         @Child Expression operand;
         @CompilationFinal boolean seenNegative;
-        
+
         Abs(Expression operand) {
             this.operand = operand;
         }
 
         @Override
         int execute(int[] arguments) {
-            int value = operand.execute(arguments); 
+            int value = operand.execute(arguments);
             if (value >= 0) {
                 return value;
             } else {
@@ -38,11 +33,9 @@ public class Demo2 {
                 return -value;
             }
         }
-        
     }
 
     static class Arg extends Expression {
-
         final int index;
 
         Arg(int index) {
@@ -53,11 +46,10 @@ public class Demo2 {
         int execute(int[] args) {
             return args[index];
         }
-
     }
 
     static class Function extends RootNode {
-
+        @SuppressWarnings("FieldMayBeFinal")
         @Child
         private Expression body;
 
@@ -75,14 +67,15 @@ public class Demo2 {
     public static void main(String[] args) {
         // Sample Program: abs(args[0])
         Function sample = new Function(new Abs(new Arg(0)));
-        CallTarget target = Truffle.getRuntime().createCallTarget(sample);
+        CallTarget target = sample.getCallTarget();
         for (int i = 0; i < 1000; i++) {
+            //noinspection PrimitiveArrayArgumentToVarargsMethod
             target.call(new int[] { 10 });
         }
         for (int i = 0; i < 1000; i++) {
+            //noinspection PrimitiveArrayArgumentToVarargsMethod
             target.call(new int[] { -10 });
         }
         System.out.println("done");
     }
-
 }
