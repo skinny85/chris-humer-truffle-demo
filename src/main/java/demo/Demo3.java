dd5faaae05adcea8b6bd0cd7dc03784b410c3428
Adam Ruka <adamruka85@gmail.com>
2024-06-29 17:03:15 -0700

    Fix demo nr 3

--- src/main/java/demo/Demo3.java
+++ src/main/java/demo/Demo3.java
@@ -1,8 +1,6 @@
 package demo;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
@@ -15,32 +13,24 @@ import demo.Demo3Factory.AbsNodeGen;
  * Same as Demo2 but now uses Truffle DSL for Abs.
  */
 public class Demo3 {
-
     abstract static class Expression extends Node {
-
         abstract int execute(VirtualFrame frame);
-        
     }
-   
+
     @NodeChild("operand")
     static abstract class Abs extends Expression {
-        
-        @CompilationFinal boolean seenNegative;
-        
         @Specialization(guards = "operand >= 0")
         int doPositive(int operand) {
             return operand;
         }
-        
+
         @Specialization(guards = "operand < 0")
         int doNegative(int operand) {
             return -operand;
         }
-        
     }
 
     static class Arg extends Expression {
-
         final int index;
 
         Arg(int index) {
@@ -51,12 +41,12 @@ public class Demo3 {
         int execute(VirtualFrame frame) {
             return ((int[])frame.getArguments()[0])[index];
         }
-
     }
 
     static class Function extends RootNode {
-
-        @Child private Expression body;
+        @SuppressWarnings("FieldMayBeFinal")
+        @Child
+        private Expression body;
 
         Function(Expression body) {
             super(null);
@@ -67,19 +57,20 @@ public class Demo3 {
         public Object execute(VirtualFrame frame) {
             return body.execute(frame);
         }
-    } 
+    }
 
     public static void main(String[] args) {
         // Sample Program: abs(args[0])
         Function sample = new Function(AbsNodeGen.create(new Arg(0)));
-        CallTarget target = Truffle.getRuntime().createCallTarget(sample);
+        CallTarget target = sample.getCallTarget();
         for (int i = 0; i < 1000; i++) {
+            //noinspection PrimitiveArrayArgumentToVarargsMethod
             target.call(new int[] { 10 });
         }
         for (int i = 0; i < 1000; i++) {
+            //noinspection PrimitiveArrayArgumentToVarargsMethod
             target.call(new int[] { -10 });
         }
         System.out.println("done");
     }
-
 }
